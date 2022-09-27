b2a425ae356df65bbd859f9e0c11552c1c2765c4
fattysun1121 <85023400+fattysun1121@users.noreply.github.com>
2022-09-26 21:39:01 -0400

    Complete logic for #knight_moves but need to format result correctly

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,30 +1,41 @@
 # frozen_string_literal: true
 
 
-# need to remove coordinate that from result
+# need to remove coordinate from result
 def knight_moves(start, destination)
   move_vectors = [1, -1].product([2, -2]) + [2, -2].product([1, -1])
-  current_level = 0
+  i = 0
   coordinate_queue = [start]
 
-  result = []
 
-  
-  coord_counter = 0
-  pop_counts = []
   loop do
-    coord = coordinate_queue.shift
-    result << coord
+    coord = coordinate_queue[i]
+    i += 1
+    
+    # generate possible coordinates, include parent coords
     possible_coords = move_vectors.map do |v| # as in vector
-      [v[0] + coord[0], v[1] + coord[1]]
-    end.select do |c| # as in coordinate
+      possible_coord = [v[0] + coord[0], v[1] + coord[1]]
+      possible_coord += coord
+    end
+    
+    # filter out out of bounds coordinates 
+    possible_coords = possible_coords.select do |c| # as in coordinate
       c[0] < 8 && c[0] >= 0 && c[1] < 8 && c[1] >= 0
     end
-    pop_counts 
+
+  
+
     coordinate_queue += possible_coords
-    return result if coord == destination
+    return coord if coord[0] == destination[0] && coord[1] == destination[1]
+
   end
 end
 
 
 p knight_moves([0, 0], [2, 4])
+
+
+# need something to hold previous coord
+# use a diff variable to traceback to the previous coord
+# coords_in_levels array to keep track of the count of coordinates
+# coordinate_queue[]
