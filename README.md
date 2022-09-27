4e4286f90f3f00aa1d07bbfc9080b26820ee69ff
fattysun1121 <85023400+fattysun1121@users.noreply.github.com>
2022-09-26 21:54:46 -0400

    Add helper method #process_coordinate to format result

---
+++ README.md
@@ -0,0 +1,5 @@
+A project on The Odin Project's Ruby on Rails track. The project specifications are the following:
+
+- Write a method #knight_moves that takes two parameters, starting coordinate and ending coordinate
+- Output the steps the knight takes to get to the destination
+- Use the idea of a graph and search methods 
--- knights_travails.rb
+++ knights_travails.rb
@@ -1,41 +1,38 @@
 # frozen_string_literal: true
 
-
-# need to remove coordinate from result
 def knight_moves(start, destination)
   move_vectors = [1, -1].product([2, -2]) + [2, -2].product([1, -1])
   i = 0
   coordinate_queue = [start]
 
-
   loop do
     coord = coordinate_queue[i]
     i += 1
-    
-    # generate possible coordinates, include parent coords
+
+    # Generate possible coordinates, include parent coordinates.
     possible_coords = move_vectors.map do |v| # as in vector
       possible_coord = [v[0] + coord[0], v[1] + coord[1]]
       possible_coord += coord
     end
-    
-    # filter out out of bounds coordinates 
+    # Filter out out of bounds coordinates.
     possible_coords = possible_coords.select do |c| # as in coordinate
       c[0] < 8 && c[0] >= 0 && c[1] < 8 && c[1] >= 0
     end
 
-  
-
     coordinate_queue += possible_coords
-    return coord if coord[0] == destination[0] && coord[1] == destination[1]
 
+    return process_coordinate(coord) if coord[0] == destination[0] && coord[1] == destination[1]
   end
 end
 
+def process_coordinate(coord)
+  result = []
+  i = coord.length - 1
+  while i >= 0
+    result << [coord[i - 1], coord[i]]
+    i -= 2
+  end
+  result
+end
 
-p knight_moves([0, 0], [2, 4])
-
-
-# need something to hold previous coord
-# use a diff variable to traceback to the previous coord
-# coords_in_levels array to keep track of the count of coordinates
-# coordinate_queue[]
+p knight_moves([0, 0], [3, 6])
